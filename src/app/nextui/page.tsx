4d06e173a335f45575eb9adde2bf45a342d7550b
심은주 <nyaknya@naver.com>
2024-12-16 11:44:23 +0900

    ✨ feat: 셀렉트 컴포넌트 추가
    - 많은일이 있었다 정말

--- src/app/nextui/page.tsx
+++ src/app/nextui/page.tsx
@@ -1,3 +1,23 @@
-export default function nextui() {
-  return <div className="container max-w-xl mx-auto p-20">테스트~</div>;
+"use client";
+
+import { Select, SelectItem } from "@nextui-org/react";
+
+export const animals = [
+  { key: "cat", label: "Cat" },
+  { key: "dog", label: "Dog" },
+  { key: "elephant", label: "Elephant" },
+];
+
+export default function NextUIExample() {
+  return (
+    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+      <Select className="max-w-xs" label="Select an animal">
+        {animals.map((animal) => (
+          <SelectItem key={animal.key} value={animal.key}>
+            {animal.label}
+          </SelectItem>
+        ))}
+      </Select>
+    </div>
+  );
 }
