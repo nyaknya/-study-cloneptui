d3f09f20fca7b7e01faac0d25e9db678f8b9a06a
심은주 <nyaknya@naver.com>
2024-12-16 11:14:29 +0900

    ✏️ chore: NextUI 세팅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,5 +16,6 @@ export default {
       },
     },
   },
+  darkMode: "class",
   plugins: [nextui()],
 } satisfies Config;
