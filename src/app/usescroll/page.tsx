b59386c99e7a2e2731dfaa25069ebcdb493a444c
심은주 <nyaknya@naver.com>
2024-12-17 11:14:55 +0900

    ✨ feat: 스크롤 프로그래스바 적용

--- src/app/usescroll/page.tsx
+++ src/app/usescroll/page.tsx
@@ -1,1 +1,23 @@
 "use client";
+import { animated, useScroll, to } from "@react-spring/web";
+
+export default function UseScrollExample() {
+  const { scrollYProgress } = useScroll();
+
+  const scrollPercentage = to(
+    scrollYProgress,
+    (value) => (value * 100).toFixed(2) + "%"
+  );
+
+  return (
+    <div>
+      <animated.div
+        className="fixed top-0 left-0 h-2 bg-red-500"
+        style={{ width: scrollPercentage }}
+      ></animated.div>
+      <div className="h-[2000px] text-center bg-gray-200 flex justify-center items-center">
+        Hello World
+      </div>
+    </div>
+  );
+}
