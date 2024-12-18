dc0878d5ca3c7f2e67b01c0d375f446cc794f729
심은주 <nyaknya@naver.com>
2024-12-18 10:32:04 +0900

    ✨ feat: 텍스트 아래에서 위로 올라오기

--- src/app/motion-test/page.tsx
+++ src/app/motion-test/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion, useSpring, useScroll } from "framer-motion";
+import { motion, useSpring, useScroll, useTransform } from "framer-motion";
 
 export default function MotionTestExample() {
   const { scrollYProgress } = useScroll();
@@ -11,6 +11,8 @@ export default function MotionTestExample() {
     restDelta: 0.001,
   });
 
+  const textTranslateY = useTransform(scrollYProgress, [0.95, 1], [200, 0]);
+
   return (
     <div className="h-[2000px] bg-gray-900">
       <motion.div
@@ -18,8 +20,27 @@ export default function MotionTestExample() {
           scale,
           transformOrigin: "center",
         }}
-        className="bg-red-500  w-[145vw] h-[300vh]  fixed -top-[100%] -left-[25%]  rounded-full"
+        className="bg-red-500 w-[145vw] h-[300vh] fixed -top-[100%] -left-[25%] rounded-full overflow-hidden"
       ></motion.div>
+
+      <motion.div className="fixed left-40 top-40 h-20 overflow-hidden">
+        <motion.div
+          style={{
+            translateY: textTranslateY,
+          }}
+          className="text-4xl font-bold text-blue-800 h-9 overflow-hidden"
+        >
+          Aha!
+        </motion.div>
+        <motion.div
+          style={{
+            translateY: textTranslateY,
+          }}
+          className="text-4xl font-bold text-blue-800 h-9 overflow-hidden"
+        >
+          You Found Me!
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
